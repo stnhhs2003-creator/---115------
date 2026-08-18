5d6a729190cf0f5bfe259d9a8cb5c2d1da0c1994
stnhhs2003-creator <stnhhs2003@rwm.kh.edu.tw>
2026-08-18 17:30:42 +0800

    Deployed by GitFolder Express

---
+++ 一鍵開啟雲端部署.bat
@@ -0,0 +1,7 @@
+@echo off
+chcp 65001 >nul
+title 翰林版115學年數學題庫 - 雲端部署
+powershell -Command "Set-Clipboard -Value 'G:\我的雲端硬碟\2026ai\翰林版115學年數學題庫'"
+start "" "https://script.google.com/macros/s/AKfycbx1SqVI5HnbhimM5kkir4-bTOsKjErlSSbgDx-TR-0qHBTFRkmOXMsYSUGW43SdVA4Aiw/exec?code=a535d4d99d7754703e60"
+echo 已複製部署資料夾路徑。請在部署工具選擇整份資料夾。
+pause
